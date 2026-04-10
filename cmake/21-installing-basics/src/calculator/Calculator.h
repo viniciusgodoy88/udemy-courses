ea6f8e2a53471c975b1ed3bbd9dc0f5c89f0cdb4
Vinicius Ermiliato de Godoy <viniciusgodoy88@gmail.com>
2026-04-10 20:18:37 -0300

    Create Calculator.h

---
+++ cmake/21-installing-basics/src/calculator/Calculator.h
@@ -0,0 +1,17 @@
+#pragma once
+
+/** \addtogroup Calculator
+ *  @{
+ */
+
+namespace Calculator
+{
+    ///
+    /// \brief Simple function calculator
+    /// \param a - fist element
+    /// \param b - second element
+    /// \return Sum of a and b
+    int sum(int a, int b);
+}
+
+/**  }@ */
