00aaab9c70ad66782324189dd541d840997b6c7a
Vinicius Ermiliato de Godoy <viniciusgodoy88@gmail.com>
2026-04-10 20:18:30 -0300

    Create ExampleClass.h

---
+++ cmake/21-installing-basics/src/application/ExampleClass.h
@@ -0,0 +1,13 @@
+//
+// Created by Slawek on 30.01.2022.
+//
+
+#ifndef PROVIDINGSOURCES_EXAMPLECLASS_H
+#define PROVIDINGSOURCES_EXAMPLECLASS_H
+
+class ExampleClass {
+public:
+  void foo();
+};
+
+#endif // PROVIDINGSOURCES_EXAMPLECLASS_H
