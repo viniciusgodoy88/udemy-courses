0cb0efb52c3083f23b9d9030963435d93134505f
Vinicius Ermiliato de Godoy <viniciusgodoy88@gmail.com>
2026-04-21 20:55:56 -0300

    Create mainEx1L1.cpp

---
+++ C++/mainEx1L1.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+//Exercicio 1 - Desenvolva um programa que escreve na tela a frase: Vou aprender a programar na Linguagem C++
+
+/* 
+int main() {
+std::cout << ("Vou aprender a programar na Linguagem C++") <<std::endl; 
+}
+
+
+
+/*
+Exercicio 2 - Altere o programa do exercício 01) para que cada palavra da frase Vou aprender a
+programar na Linguagem C++ apareça em linhas diferentes da seguinte forma:
+Vou
+aprender
+a
+programar
+na
+Linguagem C++
+
+*/
+/*int main() {
+std::cout << ("Vou") <<std::endl; 
+std::cout << ("aprender") <<std::endl; 
+std::cout << ("a") <<std::endl; 
+std::cout << ("programar") <<std::endl; 
+std::cout << ("na") <<std::endl; 
+std::cout << ("Linguagem C++") <<std::endl;
+
+	return 0;
+}
+
+/*
+Exercicio 3 - No Código do exercício 02) utilize os atalhos do Visual Studio Community 2017 para:
+Criar um comentário de uma linha de código com // colocando seu nome completo e
+data de criação do programa.
+Criar um comentário de várias linhas de código de multiplas linhas explicando e descrevendo o que
+faz e qual objetivo deste programa
+Aumentar e Diminuir o texto de seu Código Fonte
+Copiar sem selecionar a Linha de Código onde está localizado o comando std::cout e
+depois colar esta linha abaixo da mesma para que apareçam duas frases repetidas.
+Compile o Código utilizando o Atalho do Visual Studio
+Execute o Código utilizando o Atalho do Visual Studio
+Nome: Vinicius Godoy
+Data: 20/04/2026
+*/
+
+/*
+Exercicio 4 - Utilizando o comando System(COLOR CorFundoCorTexto) e altere a cor de exibição da
+fonte no terminal de saída para uma cor de sua preferência
+int main() {
+std::cout << ("Vou aprender a programar na Linguagem C++") <<std::endl; 
+system("color C7")
+}
+*/
+
